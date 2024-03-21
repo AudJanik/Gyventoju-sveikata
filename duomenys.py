eb3e8e93900940977367a6b4909bd01d58e929cd
AudJanik <163419973+AudJanik@users.noreply.github.com>
2024-03-21 12:19:06 +0200

    stulpeliu atrinkimas ir pervadinimas

--- duomenys.py
+++ duomenys.py
@@ -6,13 +6,23 @@ class Duomenys:
         self.metai = metai
         self.csv_duomenys = os.path.join('duomenys', str(metai) + '_m._atlikto_gyventojų_sveikatos_statistinio_tyrimo_duomenys.csv')
         self.csv_aprašymai = os.path.join('duomenys', str(metai) + '_m._atlikto_gyventojų_sveikatos_statistinio_tyrimo_kintamieji_ir_jų_paaiškinimai.csv')
-        self.kintamieji = []
+        self.kintamieji = ['pid', 'sex', 'age', 'citizen', 'ap', 'm_k', 'hs1', 'hs2', 'cd2', 'pn1', 'pa1', 'pe6',
+                            'sk1', 'al1']
+        self.atitikmenys = {'pid': 'ID', 'sex': 'Lytis', 'age': 'Amzius', 'citizen': 'Pilietybe',
+            'ap': 'Apskritis', 'm_k': 'Miestas/Kaimas', 'hs1': 'Bendra sveikatos bukle', 'cd2': 'Bendra dantu bukle',
+            'hs2': 'Letines ligos', 'pn1': 'Kuno skausmas', 'pa1': 'Skiepai', 'pe6': 'Sportas', 'sk1': 'Rukymas',
+            'al1': 'Alkohoilis'}
+
         self.df = pd.DataFrame()
 
-    def csv2pd(self, kintamųjų_sąrašas=[]):
+    def csv2pd(self):
         # nuskaityti csv
-        self.df = pd.read_csv(self.csv_duomenys)
+        df = pd.read_csv('duomenys/2019_m._atlikto_gyventojų_sveikatos_statistinio_tyrimo_duomenys.csv')
         # artinkti norimus kintamuosius - Audrius
+        nauja_lentele = df[self.kintamieji]
+        nauja_lentele.rename(columns=self.atitikmenys,inplace=True)
+
+
 
     def kintamojo_paaišknimas(self):
         # nebūtina, galima rankiniu būdu - Mindaugas?
