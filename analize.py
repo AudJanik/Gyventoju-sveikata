7a7a8441e200857cb5c984d27d1ce4e70e5df3b4
embar- <opensuse.lietuviu.kalba@gmail.com>
2024-03-22 12:23:25 +0200

    analizė atskiriems metams ir bendrai viems duomenims

--- analize.py
+++ analize.py
@@ -12,22 +12,28 @@ pd.set_option('display.width', 500)
 
 
 def paimti_duomenis(metai=None):
+    print('Įkeliami gyventojų sveikatos duomenys', metai if type(metai) is int else '')
     csv_bazinis_vardas = 'Sveikatos_duomenys_analizei'
-    if metai is None:
+    if metai in [None, []]:
         csv_rinkmena = f'{csv_bazinis_vardas}.csv'
-    elif len(metai) == 1:
+    elif type(metai) is int:
         csv_rinkmena = f'{csv_bazinis_vardas}_{metai}.csv'
-    else:
-        print('Nurodyti keli metai, bet neaišku, ar būtent tie metai būtų jungtiniame CSV')
+    elif type(metai) is list:
+        print('Įspėjimas: nurodyti keli metai, bet neaišku, ar būtent tie metai būtų jungtiniame CSV')
         csv_rinkmena = f'{csv_bazinis_vardas}.csv'
+    else:
+        print('Netinkamai nurodyti analizuotini metai. Nurodykite skaičių arba nenurodykite nieko visiems duomenims paimti')
+        return None
 
     if os.path.exists(csv_rinkmena):
+        print('  iš jau paruoštų csv', csv_rinkmena)
         df = pd.read_csv(csv_rinkmena)
-    elif len(metai) == 1:
+    elif type(metai) is int:
+        print('  iš pirminių csv')
         df = duomenys.Duomenys(metai).gauti_sutvarkytus_duomenis()
     else:
         print('Nerasta', csv_rinkmena)
-        df = pd.DataFrame()  # FIXME: kitoje eilutėje bus klaida, nes nebus kintamųjų
+        return None
 
     # Papildomai apskaičiuoti KMI
     df['KMI'] = df['Svoris, kg'] / ((df['Ūgis, cm'] / 100) ** 2)
@@ -37,6 +43,7 @@ def paimti_duomenis(metai=None):
 
 def bendroji_analize(df):
     # Valentina
+    print('Atliekama bendroji aprašomoji statistinė analizė...')
 
     bendras_apkaustuju_kiekis = df['ID'].count()
     # print(f'Bendras aplaustuju_skaicius: {bendras_apkaustuju_kiekis}')
@@ -124,8 +131,10 @@ def bendroji_analize(df):
     plt.ylabel('Lytis')
     plt.show()
 
-
 def koreliacine_analize(df, corr_kintamieji=[]):
+    metai = unikalus_metai(df)
+
+    print('Atliekama koreliacinė analizė...')
     # Koreliacijos analizė.
     if corr_kintamieji is None:
         corr_kintamieji = [kintamasis for kintamasis in df]
@@ -153,7 +162,8 @@ def koreliacine_analize(df, corr_kintamieji=[]):
     corr_max_lent = corr[corr.apply(abs) == stipriausia_koreliacija].dropna(how="all", axis=0).dropna(how="all", axis=1)
     # corr_max_lent1 = corr_max_lent.iloc[[0], [1]]  # 1x1 lentelė
     corr_max_kint = list(corr_max_lent.idxmax())  # du kintamieji
-    print(f'Stipriausia koreliacija rasta tarp '
+    print(f'{metai + " m. gyventojų" if type(metai) is int else "Gyventojų"} sveikatos duomenyse '
+          f'stipriausia koreliacija rasta tarp \n  '
           f'„{corr_max_kint[0]}“ ir „{corr_max_kint[1]}“ '
           f'(r = {stipriausia_koreliacija:.3f})')
     if stipriausia_koreliacija < 0.2:
@@ -161,7 +171,7 @@ def koreliacine_analize(df, corr_kintamieji=[]):
 
     plt.figure(figsize=(5, 5))
     plt.scatter(df[corr_max_kint[1]], df[corr_max_kint[0]], alpha=0.10)
-    plt.title(f'Gyventojų sveikatos duomenys:\n'
+    plt.title(f'{metai + " m. gyventojų" if type(metai) is int else "Gyventojų"} sveikatos duomenys:\n'
               f'stipriausia rasta koreliacija r={stipriausia_koreliacija:.3f}')
     plt.xlabel(corr_max_kint[1])
     plt.ylabel(corr_max_kint[0])
@@ -170,12 +180,30 @@ def koreliacine_analize(df, corr_kintamieji=[]):
     # Atlikite laiko analizę, siekiant nustatyti sveikatos rodiklių pokyčius per laiką.
     # Tai gali apimti tendencijų, sezoniškumo ir prognozių modeliavimą.
 
+def unikalus_metai(df):
+    return df['Metai'].unique().tolist()
 
 def main():
-    df = paimti_duomenis()
-    bendroji_analize(df)
     corr_kintamieji = ['Amžius', 'KMI', 'Sportas']
-    koreliacine_analize(df, corr_kintamieji)
+
+    # Analizė nepriklausomai nuo metų
+    df = paimti_duomenis()
+    if df is not None:
+        print()
+        print(f'ANALIZUOJAMI METAI IMANT DUOMENIS KARTU: {", ".join(str(metai) for metai in unikalus_metai(df))}')
+        bendroji_analize(df)
+        koreliacine_analize(df, corr_kintamieji)
+
+    for metai in [2014, 2019]:
+        print()
+        print()
+        df = paimti_duomenis(metai)
+        if df is not None:
+            print()
+            print(f'ANALIZUOJAMI METAI IMANT DUOMENIS ATSKIRAI: {unikalus_metai(df)}')
+            print()
+            bendroji_analize(df)
+            koreliacine_analize(df, corr_kintamieji)
 
 
 if __name__ == '__main__':
