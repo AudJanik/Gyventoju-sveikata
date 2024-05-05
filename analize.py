b32c559fcc8d8f5138851a0b189d3887320d4891
embar- <opensuse.lietuviu.kalba@gmail.com>
2024-05-05 17:42:20 +0300

    main f-ja

--- analize.py
+++ analize.py
@@ -1,5 +1,11 @@
 import analize_bendroji_ir_klasterine
 import klasifikacija_pg_fizini_aktyvuma
 
-analize_bendroji_ir_klasterine.main()
-klasifikacija_pg_fizini_aktyvuma.main()
+
+def main():
+    analize_bendroji_ir_klasterine.main()
+    klasifikacija_pg_fizini_aktyvuma.main()
+
+
+if __name__ == '__main__':
+    main()
